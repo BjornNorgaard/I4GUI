e318aef0ab5b3af35f4816d0e23dfdb33c370220
Nørgaard Laptop VS <Bjornsnorgaard@gmail.com>
2016-02-17 12:01:25 +0100

    doing algorithme for finding top10, keep getting outofboundexception

--- 05 - Baby names/05 - Baby names/MainWindow.xaml.cs
+++ 05 - Baby names/05 - Baby names/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,6 +22,9 @@ namespace _05___Baby_names
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<BabyName> CollectionOfBabyNames = new List<BabyName>();
+        List<BabyName> Top10BabyNames = new List<BabyName>(10); 
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,29 +32,78 @@ namespace _05___Baby_names
 
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
-            #region Loading Raw babynamedata into Top10ListBox
+            #region Loading Raw babynamedata into BabyName class
 
-            List<string> CollectionOfBabyNames = new List<string>();
+            string[] lines = File.ReadAllLines("babynames.txt");    // used as temporary while loading
 
-            string[] lines = File.ReadAllLines("babynames.txt");
-
-            // load ALL the names!
+            // Convert raw data into BabyName class
             for (int i = 0; i < lines.Length; i++)
             {
-                CollectionOfBabyNames.Add(lines[i]);
+                CollectionOfBabyNames.Add(new BabyName(lines[i]));
             }
-
-            Top10listBox.ItemsSource = CollectionOfBabyNames;
 
             #endregion
 
+            FindTop10(1950);
+            Top10listBox.ItemsSource = Top10BabyNames;
+
+            #region Loading decades into DecadesListBox
+
             List<string> CollectionOfDecades = new List<string>();
-            for (int i = 1900; i <= 2000; i+=10)
+            for (int i = 1900; i <= 2000; i += 10)
             {
                 CollectionOfDecades.Add(i.ToString());
             }
 
             DecadeslistBox.ItemsSource = CollectionOfDecades;
+
+            #endregion
+        }
+
+        public void FindTop10(int y)
+        {
+            // fill top10list with null babies all ranked last
+            for (int i = 0; i < Top10BabyNames.Count; i++)
+            {
+                Top10BabyNames.Add(new BabyName("Derpo 0 0 0 0 0 0 0 0 0 0 0"));
+            }
+
+            BabyName babyName;
+            
+            for (int i = 0; i < CollectionOfBabyNames.Count; i++)
+            {
+                babyName = CollectionOfBabyNames[i];
+
+                if (babyName.Rank(y) > Top10BabyNames[Top10BabyNames.Count-1].Rank(y))
+                {
+                    Top10BabyNames[9] = babyName;
+                }
+
+                #region Own sorting algorithme
+
+                for (int j = 8; j >= 0; j--)
+                {
+                    if (Top10BabyNames[j + 1].Rank(y) > Top10BabyNames[j].Rank(y))
+                    {
+                        SwapTop10Babies(j + 1, j);
+                    }
+                }
+
+                #endregion
+
+                //#region With List<>.Sort
+
+                //Top10BabyNames.Sort((x, z) => x.Rank(y).CompareTo(z.Rank(y)));
+
+                //#endregion
+            }
+        }
+
+        public void SwapTop10Babies(int indexOfBabyToGoUp, int indexOfBabyToGoDown)
+        {
+            BabyName temp = Top10BabyNames[indexOfBabyToGoDown];
+            Top10BabyNames[indexOfBabyToGoDown] = Top10BabyNames[indexOfBabyToGoUp];
+            Top10BabyNames[indexOfBabyToGoUp] = Top10BabyNames[indexOfBabyToGoDown];
         }
     }
 }
