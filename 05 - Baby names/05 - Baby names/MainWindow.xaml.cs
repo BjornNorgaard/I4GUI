6a7de23e3d41e87bdefdf9f95dd1ec32d026ff63
Bjørn Tower <bjornsnorgaard@gmail.com>
2016-02-17 20:31:39 +0100

    somehow the listbox is not updated when the source is...

--- 05 - Baby names/05 - Baby names/MainWindow.xaml.cs
+++ 05 - Baby names/05 - Baby names/MainWindow.xaml.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _05___Baby_names
 {
@@ -23,7 +14,8 @@ namespace _05___Baby_names
     public partial class MainWindow : Window
     {
         List<BabyName> CollectionOfBabyNames = new List<BabyName>();
-        List<BabyName> Top10BabyNames = new List<BabyName>(10); 
+        List<BabyName> Top20Babies = new List<BabyName>(20);
+        List<string> Top10BabyNames = new List<string>(10);
 
         public MainWindow()
         {
@@ -55,11 +47,41 @@ namespace _05___Baby_names
             DecadeslistBox.ItemsSource = CollectionOfDecades;
 
             #endregion
+
+            DetermineTop10BabyNamesForGivenYear(2000);
         }
 
         public void DetermineTop10BabyNamesForGivenYear(int y)
         {
-            // all was poop, starting over
+            foreach (BabyName baby in CollectionOfBabyNames)
+            {
+                if (baby.Rank(y) < 11 && baby.Rank(y) != 0)
+                {
+                    Top20Babies.Add(baby);
+                }
+            }
+
+            Top20Babies.Sort((a,b) => a.Rank(y).CompareTo(b.Rank(y)));
+            Top10BabyNames.Clear();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Top10BabyNames.Add(i+1 + " " + Top20Babies[i].Name + " and " + Top20Babies[i+1].Name);
+            }
+
+            Top10listBox.ItemsSource = Top10BabyNames;
+            Debug.WriteLine("Source changed!");
+        }
+
+        private void DecadeslistBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DetermineTop10BabyNamesForGivenYear((DecadeslistBox.SelectedIndex)*10+1900);
+            //Top10listBox.UpdateLayout();
+            Debug.WriteLine("DecadeslistBox_OnSelectionChanged called!");
+        }
+
+        private void Top10listBox_OnSourceUpdated(object sender, DataTransferEventArgs e)
+        {
         }
     }
 }
