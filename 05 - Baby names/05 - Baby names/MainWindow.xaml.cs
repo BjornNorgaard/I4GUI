b5929fea61bb4e14dd3464c77645eee1d288f76b
Nørgaard Laptop VS <Bjornsnorgaard@gmail.com>
2016-02-17 12:18:49 +0100

    all was poop, starting the 'FindTop10' over

--- 05 - Baby names/05 - Baby names/MainWindow.xaml.cs
+++ 05 - Baby names/05 - Baby names/MainWindow.xaml.cs
@@ -43,10 +43,7 @@ namespace _05___Baby_names
             }
 
             #endregion
-
-            FindTop10(1950);
-            Top10listBox.ItemsSource = Top10BabyNames;
-
+            
             #region Loading decades into DecadesListBox
 
             List<string> CollectionOfDecades = new List<string>();
@@ -60,50 +57,9 @@ namespace _05___Baby_names
             #endregion
         }
 
-        public void FindTop10(int y)
+        public void DetermineTop10BabyNamesForGivenYear(int y)
         {
-            // fill top10list with null babies all ranked last
-            for (int i = 0; i < Top10BabyNames.Count; i++)
-            {
-                Top10BabyNames.Add(new BabyName("Derpo 0 0 0 0 0 0 0 0 0 0 0"));
-            }
-
-            BabyName babyName;
-            
-            for (int i = 0; i < CollectionOfBabyNames.Count; i++)
-            {
-                babyName = CollectionOfBabyNames[i];
-
-                if (babyName.Rank(y) > Top10BabyNames[Top10BabyNames.Count-1].Rank(y))
-                {
-                    Top10BabyNames[9] = babyName;
-                }
-
-                #region Own sorting algorithme
-
-                for (int j = 8; j >= 0; j--)
-                {
-                    if (Top10BabyNames[j + 1].Rank(y) > Top10BabyNames[j].Rank(y))
-                    {
-                        SwapTop10Babies(j + 1, j);
-                    }
-                }
-
-                #endregion
-
-                //#region With List<>.Sort
-
-                //Top10BabyNames.Sort((x, z) => x.Rank(y).CompareTo(z.Rank(y)));
-
-                //#endregion
-            }
-        }
-
-        public void SwapTop10Babies(int indexOfBabyToGoUp, int indexOfBabyToGoDown)
-        {
-            BabyName temp = Top10BabyNames[indexOfBabyToGoDown];
-            Top10BabyNames[indexOfBabyToGoDown] = Top10BabyNames[indexOfBabyToGoUp];
-            Top10BabyNames[indexOfBabyToGoUp] = Top10BabyNames[indexOfBabyToGoDown];
+            // all was poop, starting over
         }
     }
 }
