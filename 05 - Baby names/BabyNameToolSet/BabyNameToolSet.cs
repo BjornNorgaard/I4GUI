9d79b1e1656ef6bf6112aad276ca68a38c877622
Bjørn Tower <bjornsnorgaard@gmail.com>
2016-03-06 19:10:21 +0100

    tast 1 and 2 done

---
+++ 05 - Baby names/BabyNameToolSet/BabyNameToolSet.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace BabyNameToolSet
+{
+    public class BabyNameToolSet
+    {
+        #region Variables and Properties
+
+        public List<BabyName> TotalListOfBabyNames { get; } = new List<BabyName>();
+        readonly List<BabyName> _top20BothGenderBabyNames = new List<BabyName>();
+
+        public List<string> Top10List { get; } = new List<string>();
+
+        readonly string _pathToBabyNameFile = "babynames.txt";
+
+        #endregion
+
+        #region Constructors
+
+        public BabyNameToolSet()
+        {
+            LoadBabyNamesIntoCollection(_pathToBabyNameFile);
+        }
+
+        public BabyNameToolSet(string pathToBabyNameFile)
+        {
+            _pathToBabyNameFile = pathToBabyNameFile;
+
+            LoadBabyNamesIntoCollection(_pathToBabyNameFile);
+        }
+
+        #endregion
+
+        private void LoadBabyNamesIntoCollection(string pathToBabyNameFile)
+        {
+            string[] lines = System.IO.File.ReadAllLines(_pathToBabyNameFile);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                TotalListOfBabyNames.Add(new BabyName(lines[i]));
+            }
+        }
+
+        public void DetermineTop10ForGivenYear(int year)
+        {
+            List<BabyName> SortedList = TotalListOfBabyNames.OrderBy(o => o.Rank(year)).ToList();
+            _top20BothGenderBabyNames.Clear();
+            Top10List.Clear();
+
+            for (int i = 0; i < 20; i++)
+            {
+                _top20BothGenderBabyNames.Add(SortedList[i]);
+            }
+
+            for (int i = 0; i <= 20; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Top10List.Add(i + ". " + _top20BothGenderBabyNames[i] + " and ");
+                }
+                else if (i % 2 != 0)
+                {
+                    Top10List[i - 1] += _top20BothGenderBabyNames[i];
+                }
+            }
+        }
+    }
+}
--- 06 - AgentAssignment/AgentAssignment/AgentAssignment/MainWindow.xaml.cs
+++ 06 - AgentAssignment/AgentAssignment/AgentAssignment/MainWindow.xaml.cs
@@ -10,22 +10,16 @@ namespace AgentAssignment
     /// </summary>
     public partial class MainWindow : Window
     {
+        Agents _agents = new Agents();
+
         public MainWindow()
         {
             InitializeComponent();
 
-            Binding id_binding = new Binding();
-            id_binding.Source = _agent;
-            id_binding.Path = new PropertyPath("_agent.Id");
-            IdTextBlock.SetBinding(TextBox.TextProperty, id_binding);
-        }
+            _agents.Add(new Agent("666", "Sir", "Derping", "Engineering"));
+            _agents.Add(new Agent("006", "Jaymes", "Blond", "Singing"));
 
-        Agent _agent = new Agent("007", "James Bond", "Assasinations", "Danish Parlament");
-
-        private void IdTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            _agent.Id = IdTextBox.Text;
-            IdTextBlock.Text = _agent.Id;
+            Grid.DataContext = _agents;
         }
     }
 }
