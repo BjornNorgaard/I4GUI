76fc7d3e980c54bb16ce98af5bc901d9932b38cc
Norgaard Tower <bjornsnorgaard@gmail.com>
2016-06-07 20:10:57 +0200

    sommer eksamen proj is broke

--- Eksamen/Tidligere eksamensopgaver/GUI eksamen sommer 2015/GUI eksamen sommer 2015/Views/CreateWindow.xaml.cs
+++ Eksamen/Tidligere eksamensopgaver/GUI eksamen sommer 2015/GUI eksamen sommer 2015/Views/CreateWindow.xaml.cs
@@ -10,22 +10,21 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace GUI_eksamen_sommer_2015
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CreateWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CreateWindow : Window
     {
-        List<Tuple<DateTime, string>> _medList = new List<Tuple<DateTime, string>>();
+        public DateTime MedDateTime { get; set; }
+        public string MedDescription { get; set; }
 
-        public MainWindow()
+        public CreateWindow()
         {
             InitializeComponent();
         }
-        
     }
 }
