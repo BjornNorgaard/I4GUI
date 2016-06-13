35db45c4a95ef51967c66c08eeb6b1d28e58a7b8
Norgaard Tower <bjornsnorgaard@gmail.com>
2016-06-14 01:00:07 +0200

    search works

--- Eksamen/Eksamensopgave/I4GUI eksamen 2016 sommer/I4GUI eksamen 2016 sommer-Web/Controllers/SearchController.cs
+++ Eksamen/Eksamensopgave/I4GUI eksamen 2016 sommer/I4GUI eksamen 2016 sommer-Web/Controllers/SearchController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,17 @@ namespace I4GUI_eksamen_2016_sommer_Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Search(string tags)
+        {
+            using (StreamWriter sw = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\.joke_search"))
+            {
+                sw.Write(tags);
+                sw.Close();
+            }
+
+            return View("Index");
         }
     }
 }
