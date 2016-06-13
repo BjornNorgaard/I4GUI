2b59a6baefa5254e8d690dbc99aa2e1ae3d2d92f
Norgaard Tower <bjornsnorgaard@gmail.com>
2016-06-13 10:05:29 +0200

    made ctor for joke class

--- Eksamen/Eksamensopgave/I4GUI eksamen 2016 sommer/I4GUI eksamen 2016 sommer/Joke.cs
+++ Eksamen/Eksamensopgave/I4GUI eksamen 2016 sommer/I4GUI eksamen 2016 sommer/Joke.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace I4GUI_eksamen_2016_sommer
 {
+    [Serializable]
     public class Joke
     {
         #region Properties
@@ -13,13 +15,21 @@ namespace I4GUI_eksamen_2016_sommer
         public string Name { get; set; }
         public string Date { get; set; }
         public string Source { get; set; }
-        public List<string> TopicsList { get; set; }
+        public List<string> TagListList { get; set; }
 
         #endregion
 
+        public Joke(string jName, string jDate, string jSource, List<string> jTagList)
+        {
+            Name = jName;
+            Date = jDate;
+            Source = jSource;
+            TagListList = jTagList;
+        }
+
         public bool ContainsTopic(string topic)
         {
-            foreach (string item in TopicsList)
+            foreach (string item in TagListList)
             {
                 if (item.Contains(topic))
                 {
