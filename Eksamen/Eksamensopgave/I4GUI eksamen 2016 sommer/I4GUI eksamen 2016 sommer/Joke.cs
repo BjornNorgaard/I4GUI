fdd62fff3336694f3a5feb05db0cb8d76a81f67e
Norgaard Tower <bjornsnorgaard@gmail.com>
2016-06-13 09:56:17 +0200

    made properties for joke class

---
+++ Eksamen/Eksamensopgave/I4GUI eksamen 2016 sommer/I4GUI eksamen 2016 sommer/Joke.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4GUI_eksamen_2016_sommer
+{
+    public class Joke
+    {
+        #region Properties
+
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Source { get; set; }
+        public List<string> TopicsList { get; set; }
+
+        #endregion
+
+    }
+}
