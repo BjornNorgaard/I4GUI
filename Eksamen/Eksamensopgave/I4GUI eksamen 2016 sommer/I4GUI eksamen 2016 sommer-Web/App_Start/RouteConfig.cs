402690ba81aefae244dd2056e3c85add44b7d3a1
Norgaard Tower <bjornsnorgaard@gmail.com>
2016-06-13 22:56:25 +0200

    changed index

--- Eksamen/Eksamensopgave/I4GUI eksamen 2016 sommer/I4GUI eksamen 2016 sommer-Web/App_Start/RouteConfig.cs
+++ Eksamen/Eksamensopgave/I4GUI eksamen 2016 sommer/I4GUI eksamen 2016 sommer-Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace I4GUI_eksamen_2016_sommer_Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Create", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
