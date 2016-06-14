fc8f7ba9b498b71c99591812d768130d3cde3867
Norgaard Tower <bjornsnorgaard@gmail.com>
2016-06-14 02:31:08 +0200

    made new release version and fixed some bugs with web

---
+++ Eksamen/Solutions til release/02 - Uden login, improved/I4GUI eksamen 2016 sommer/I4GUI eksamen 2016 sommer-Web/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace I4GUI_eksamen_2016_sommer_Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Browse", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
